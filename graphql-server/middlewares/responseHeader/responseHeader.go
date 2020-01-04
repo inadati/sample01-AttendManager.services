0e0cd536c6ae3006a7ecc525fa34cd608cc3cb51
Inadati <inadati@itayahiroakinoMacBook-Pro.local>
2020-01-04 18:59:57 +0900

    Access-Control-Allow-Origin 変更

--- graphql-server/middlewares/responseHeader/responseHeader.go
+++ graphql-server/middlewares/responseHeader/responseHeader.go
@@ -8,7 +8,7 @@ func Middleware() func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			w.Header().Set("Content-Type", "application/json")
-			w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+			w.Header().Set("Access-Control-Allow-Origin", "https://attend-manager-sample.cf")
 			w.Header().Set("Access-Control-Allow-Headers", "authorization,content-type")
 			next.ServeHTTP(w, r)
 		})
